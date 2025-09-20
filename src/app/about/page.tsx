820a36812b802515a805439faa04917f376aa5ea
shahil-kv <mshahilkv@gmail.com>
2025-09-20 17:26:44 +0530

    firebase things are sorted

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <h1>about</h1>;
+};
+
+export default page;
---
+++ src/app/skills/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <h1>skills</h1>;
+};
+
+export default page;
